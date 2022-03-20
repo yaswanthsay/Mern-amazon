e63e709adc6d06931eb5a6014f107e5e3eaf238a
krishna <yaswanthkrishna34@gmail.com>
2022-03-20 21:41:49 +0530

    Create-react-app

---
+++ amazon/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  )
+}
+
+export default App
